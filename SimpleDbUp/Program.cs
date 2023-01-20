d404939bc28349caa0f84479fe1882885840155b
Hamid Mayeli <hamid.mayeli@justeattakeaway.com>
2023-01-20 23:22:17 +0000

    Init project with command line

---
+++ SimpleDbUp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+
+var command = new RootCommand();
+
+var scriptDirectory = new Option<string>(new[] { "-s", "--scripts" }, () => Directory.GetCurrentDirectory(), "Source scripts directory");
+
+var connectionString = new Option<string>(new[] { "-c", "--connection-string" }, description: "Connection string to target database");
+connectionString.IsRequired = true;
+
+command.AddOption(connectionString);
+command.AddOption(scriptDirectory);
+
+command.SetHandler((conStr, scripts) => Console.WriteLine((conStr + "-" + scripts) ?? "Null"), connectionString, scriptDirectory);
+
+var parser = new CommandLineBuilder(command)
+    .UseHelp()
+    .UseDefaults()
+    .Build();
+
+await parser.InvokeAsync(args);
